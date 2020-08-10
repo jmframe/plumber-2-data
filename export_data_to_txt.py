75e9023e20cab56608c273bb485dd98983a1b54e
Jonathan Frame <jmframe@crimson.ua.edu>
2020-08-10 16:19:20 -0400

    vegetation

--- export_data_to_txt.py
+++ export_data_to_txt.py
@@ -10,9 +10,11 @@ import glob
 
 meta = {'flux':['x', 'y', 'time'], 'met':['x','y','time','longitude', 'elevation']}
 varz = {'flux':['NEE', 'GPP', 'Qle', 'Qh', 'Qg'], 
-        'met':['Tair', 'SWdown', 'LWdown', 'VPD', 'Qair', 'Psurf', 'Precip', 'Wind', 'RH', 'CO2air']}
-timez = ['year', 'month', 'day', 'hour', 'minute', 'datetime']
-vegs = {'flux':['IGBP_veg_long'], 'met':['IGBP_veg_long','LAI_alternative','LAI']}
+        'met':['Tair', 'SWdown', 'LWdown', 'VPD', 'Qair',
+               'Psurf', 'Precip', 'Wind', 'RH', 'CO2air',
+               'LAI_alternative','LAI']}
+timez = ['datetime', 'year', 'month', 'day', 'hour', 'minute']
+vegs = {'flux':['IGBP_veg_long'], 'met':['IGBP_veg_long']}
 
 data_dir = '/discover/nobackup/jframe/data/'
 directory = {'flux':data_dir+'plumber-2-flux/', 'met':data_dir+'plumber-2-met/'}
@@ -24,9 +26,32 @@ for fm in flux_met:
         n_files[fm] += 1
         files[fm].append(filepath.split('/')[-1])
 
+# Open and read the txt file with all the unique veg values
+IGBP_veg_unique = []
+with open('IGBP_veg_long.txt', 'r') as f:
+      for line in f:
+          a = line[:-1]
+          IGBP_veg_unique.append(a)
+
+# This is a long and dumb function to get the string values out of the masked array for IGBP
+def get_igbp(veg_ma):
+    S = [str(veg_ma[i].data).split(('\'|b')[0])[1] for i in range(200)]
+    s = ''
+    old_i = None
+    for i in S:
+        s = s + i
+        old_i = i
+        if i == '' and old_i == '':
+            break
+    s_ = ''
+    for i in s:
+        if i.isalpha():
+            s_ = s_ + i
+    return s_
+
 # Set up data, this is goping to get a tad bit confusing...
 # A disctionary of two dictionaries, each with pandas dataframes for each data file
-data = {fm:{ifile:pd.DataFrame(columns=varz[fm]+timez) for ifile in files[fm]} for fm in flux_met}
+data = {fm:{ifile:pd.DataFrame(columns=timez+varz[fm]+['IGBP_veg_long']) for ifile in files[fm]} for fm in flux_met}
 
 # Now the main loop where we extract the data from the NetCDF masked array
 # And fill in our dictionary of two dictionaries with pandas dataframes
@@ -54,8 +79,14 @@ for fm in flux_met:
             #if qc_flux_var in list(d.variables.keys()):
             #    flux_data[flux_file][iflux_var] = d[qc_flux_var][:].tolist(-999)
     
-            # TODO: convert the vegetation data to numeric values
+            # Add vegetation data.
+            # Need to convert the vegetation data to numeric values.
+            v = get_igbp(d['IGBP_veg_long'])
+            for i, iv in enumerate(IGBP_veg_unique):
+                if v == iv:
+                    data[fm][ifile]['IGBP_veg_long'] = i
             
+
         # Add dates to Pandas
         dtime = netCDF4.num2date(d.variables['time'][:],d.variables['time'].units)
         dtime = np.ma.getdata(dtime)
