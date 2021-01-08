6c870a81bc555e923c1162c6bc5e4d4e37b64dd5
Jonathan Frame <jmframe@crimson.ua.edu>
2021-01-07 22:54:28 -0500

    print variables

--- print_variables.py
+++ print_variables.py
@@ -9,7 +9,7 @@ import datetime as dt
 import glob
 
 #flux_directory = '/discover/nobackup/jframe/data/flux-evaluation-20200722T145656Z-001/'
-flux_directory = '/discover/nobackup/jframe/data/plumber-2-met/'
+flux_directory = '/discover/nobackup/jframe/data/plumber-2/met-nc/'
 flux_files = []
 n_flux_files = 0
 for filepath in glob.iglob(flux_directory+'*.nc'):
@@ -25,6 +25,8 @@ for filepath in glob.iglob(flux_directory+'*.nc'):
     flux_time_min_max[flux_file] = [min(d['time']),max(d['time'])]
     flux_variables[flux_file] = list(d.variables.keys())
     count_files += 1
+    print(d['time'])
+    break
     print(flux_file, "file {} out of {}".format(count_files, n_flux_files))
     print("minimum time: {} and maximum time{}".format(min(d['time']),max(d['time'])))
     print(flux_variables[flux_file])
@@ -34,15 +36,16 @@ for filepath in glob.iglob(flux_directory+'*.nc'):
 
 
 # Check to see what variables are in all the files, if any.
-for i, flux_file in enumerate(flux_variables.keys()):
-    current_variables = flux_variables[flux_file]
-    if i == 0:
-        variable_list = current_variables
-    else:
-        for variable in variable_list:
-            if variable not in current_variables:
-                variable_list.remove(variable)
-print(variable_list) # ['x', 'y', 'time', 
+#for i, flux_file in enumerate(flux_variables.keys()):
+#    current_variables = flux_variables[flux_file]
+#    if i == 0:
+#        variable_list = current_variables
+#    else:
+#        for variable in variable_list:
+#            if variable not in current_variables:
+#                variable_list.remove(variable)
+#    break
+#print(variable_list) # ['x', 'y', 'time', 
                      # 'NEE', 'GPP', 'NEE_qc', 'Qle', 'Qle_qc', 'Qh', 'Qh_qc', 'Qg_qc', 
                      # 'Ustar', 'Ustar_qc', 'latitude', 'longitude', 'elevation', 'IGBP_veg_long'] 
 # check the unbroken time periods for the variables
